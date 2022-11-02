968f05198de08b08af79b9da72e1f865780769fa
Luis Henrique <luique16@outlook.com>
2022-11-01 20:36:40 -0400

    “ ✨ Funcionalidade:  Adicionar Tarefa”.

--- src/js/lista-tarefas.js
+++ src/js/lista-tarefas.js
@@ -1,2 +1,20 @@
 import './nav.js'
 import '../css/lista-tarefas.css'
+
+let nova_trf = document.querySelector('#input-nova-tarefa');
+let btn_add = document.querySelector('#btn-adicionar');
+let lista_trfs = document.querySelector('#lista-tarefas');
+
+btn_add.addEventListener('click',function(btn){
+   let input_value = document.querySelector('#input-nova-tarefa').value
+
+    document.createElement('li')
+
+    nova_trf.className = 'tarefa'
+    
+   nova_trf.innerHTML = `
+	<div>${input_value}</div>
+  <button class="btn-excluir">Excluir</button>
+`
+lista_trfs.appendChild(nova_trf)
+})
