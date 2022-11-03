1d3cd9055a436ae16f8d05a3273e8c729a478035
Luis Henrique <luique16@outlook.com>
2022-11-03 17:39:39 -0400

    arrumando codigo

--- src/js/formulario-pagamento.js
+++ src/js/formulario-pagamento.js
@@ -2,3 +2,10 @@ import './nav.js'
 import '../css/formulario-pagamento.css'
 
 
+let numero_do_cartao_input = document.querySelector("#nome")
+let btn = document.querySelector(".btn-confirmar")
+
+btn.addEventListener("click",function(){
+    console.log(numero_do_cartao_input)
+})
+
--- src/js/lista-tarefas.js
+++ src/js/lista-tarefas.js
@@ -13,21 +13,20 @@ btn_add.addEventListener("click", function () {
   nova_li.innerHTML = `
 	<div>${input_value}</div>
 `;
-lista_trfs.appendChild(nova_li);
+
+  lista_trfs.appendChild(nova_li);
 
   let btn_excluir = document.createElement("button");
-  btn_excluir.setAttribute('id','deu')
-  let texto = document.createTextNode("Excluir")
-btn_excluir.appendChild(texto)
+  btn_excluir.setAttribute("id", "deu");
+  let texto = document.createTextNode("Excluir");
+  btn_excluir.appendChild(texto);
   btn_excluir.className = "btn-excluir";
 
-nova_li.appendChild(btn_excluir)
-
+  nova_li.appendChild(btn_excluir);
 
   btn_excluir.addEventListener("click", function (event) {
     let tarefa = btn_excluir.parentElement;
     lista_trfs.removeChild(tarefa);
     console.log("hwujhdu");
   });
-
 });
