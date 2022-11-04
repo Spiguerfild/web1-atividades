7ad6773f338e16fd08d2a6af7b612c31f633205e
Luis Henrique <luique16@outlook.com>
2022-11-03 22:40:33 -0400

    atualizando codigo cartao

--- src/js/formulario-pagamento.js
+++ src/js/formulario-pagamento.js
@@ -2,10 +2,13 @@ import './nav.js'
 import '../css/formulario-pagamento.css'
 
 
-let numero_do_cartao_input = document.querySelector("#nome")
-let btn = document.querySelector(".btn-confirmar")
+const numero_do_cartao_input = document.querySelector("#numero-cartao")
+const nome_input  = document.querySelector
+const btn_confirmar = document.querySelector(".btn-confirmar")
+const card_number = document.querySelector('.card-number')
 
-btn.addEventListener("click",function(){
-    console.log(numero_do_cartao_input)
+
+numero_do_cartao_input.addEventListener("input",function(){
+card_number.textContent = numero_do_cartao_input.value
 })
 
