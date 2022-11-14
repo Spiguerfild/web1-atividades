3344330703ab17b6b3be2035a69bc33c7ae8f0ca
Luis Henrique <luique16@outlook.com>
2022-11-14 15:08:42 -0400

    formulario pagamento quase concluido

--- src/js/formulario-pagamento.js
+++ src/js/formulario-pagamento.js
@@ -2,13 +2,34 @@ import './nav.js'
 import '../css/formulario-pagamento.css'
 
 
-const numero_do_cartao_input = document.querySelector("#numero-cartao")
-const nome_input  = document.querySelector
-const btn_confirmar = document.querySelector(".btn-confirmar")
-const card_number = document.querySelector('.card-number')
+/*1 inp*/const numero_do_cartao_input = document.querySelector('#numero-cartao');
+/*2 inp*/const name_input  = document.querySelector('#nome');
+/*3 inp*/const data_input = document.querySelector('#data-vencimento');
+/*4 inp*/const codigo_seguranca_input = document.querySelector('#codigo-seguranca');
+
+numero_do_cartao_input.setAttribute('placeholder','Número');
+name_input.setAttribute('placeholder','Nome');
 
 
-numero_do_cartao_input.addEventListener("input",function(){
-card_number.textContent = numero_do_cartao_input.value
-})
+/*1 out*/const card_number = document.querySelector('.card-number');
+/*2 out*/const card_name  = document.querySelector('.card-holder');
+/*3 out*/const card_data = document.querySelector('.expiration');
+/*4 out const card_codigo_seguranca = document.querySelector('#naoseiainda');*/
 
+
+const btn_confirmar = document.querySelector('.btn-confirmar');
+
+/*chamadas de eventos---------------------------------------------------------👇*/ 
+
+numero_do_cartao_input.addEventListener('input',function(){
+card_number.textContent = numero_do_cartao_input.value;
+})/*numero cartao👆*/
+
+
+name_input.addEventListener('input',function(){
+    card_name.textContent = name_input.value;
+})/*nome cartao👆*/
+
+data_input.addEventListener('input',function(){
+    card_data.textContent = data_input.value; 
+})/*data cartao👆*/
